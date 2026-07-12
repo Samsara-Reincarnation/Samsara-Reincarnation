2562128f9030e38387bc059503969821c641995c
Shiny Metagross <andy.clarke99.ac@gmail.com>
2026-07-12 11:53:15 -0700

    Update build_pk3.bat
    - You know what this means...

--- build_pk3.bat
+++ build_pk3.bat
@@ -1,7 +1,7 @@
 @echo off
 
 REM file name of the built PK3, feel free to edit - Stolen from COG lol
-set pk3name="SamsaraReincarnation_v2.2.1-Dev-Build"
+set pk3name="SamsaraReincarnation_v3.0-Dev-Build"
 
 
 
